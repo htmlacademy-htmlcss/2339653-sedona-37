c9948768b91e37868ea1d1683e7a9dd5086febdf
Andrey Yalandaev <oeb72umvd@mail.ru>
2023-06-29 15:56:27 +0300

    Исправляет JS для открытия/закрытия модал. окна

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let modalWindow = document.querySelector('.modal-container');
 let modalForm = document.querySelector('.modal-window');
-let buttonModalOpen = document.querySelector('.modal-window-open');
+let buttonModalOpen = document.querySelector('.modal-window-open-button');
 let buttonModalClose = document.querySelector('.modal-close-button');
 
 buttonModalOpen.onclick = function() {
-    modalWindow.style.display = 'flex';
+    modalWindow.classList.add('modal-window-open');
 }
 
 buttonModalClose.onclick = function() {
-    modalWindow.style.display = 'none';
+    modalWindow.classList.remove('modal-window-open');
 }
 
 modalWindow.onclick = function() {
-     modalWindow.style.display = 'none';
+    modalWindow.classList.remove('modal-window-open');
 }
 
 modalForm.onclick = function(event) {
